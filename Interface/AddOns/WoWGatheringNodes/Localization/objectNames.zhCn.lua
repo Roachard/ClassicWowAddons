abe80e8a8ddae28586c07be36662230d2892c6ca
Js <JimmyLoveSiren@users.noreply.github.com>
2019-10-21 09:51:13 +0900

    GatherMate2

---
+++ Interface/AddOns/WoWGatheringNodes/Localization/objectNames.zhCn.lua
@@ -0,0 +1,145 @@
+-- Please use the Localization App on WoWAce to Update this
+-- https://wow.curseforge.com/projects/wowgatheringnodes/localization
+
+if GetLocale() ~= "zhCn" then return; end
+local WoWGatheringNodes = LibStub("AceAddon-3.0"):GetAddon("WoWGatheringNodes")
+local L = LibStub("AceLocale-3.0"):NewLocale("WoWGatheringNodes", "zhCn")
+if not L then return end
+
+--[[Translation missing --]]
+--[[ L["Auto Import Data to Gathermate"] = "Auto Import Data to Gathermate"--]] 
+--[[Translation missing --]]
+--[[ L["Auto Import New Data"] = "Auto Import New Data"--]] 
+--[[Translation missing --]]
+--[[ L["Auto Import to Data Gathermate"] = "Auto Import Data to Gathermate"--]] 
+--[[Translation missing --]]
+--[[ L["Auto Import to Herb Data to Carbonite"] = "Auto Import to Herb Data to Carbonite"--]] 
+--[[Translation missing --]]
+--[[ L["Auto Import to Mine Data to Carbonite"] = "Auto Import to Mine Data to Carbonite"--]] 
+--[[Translation missing --]]
+--[[ L["Automaticaly imports data when updated data is found"] = "Automaticaly imports data when updated data is found"--]] 
+--[[Translation missing --]]
+--[[ L["Clear Data"] = "Clear Data"--]] 
+--[[Translation missing --]]
+--[[ L["Clears data from memory if version has already been imported."] = "Clears data from memory if version has already been imported."--]] 
+--[[Translation missing --]]
+--[[ L["Custom Objects"] = "Custom Objects"--]] 
+--[[Translation missing --]]
+--[[ L["Enable Custom Objects"] = "Enable Custom Objects"--]] 
+--[[Translation missing --]]
+--[[ L["Failed to load WoWGatheringNodes due to "] = "Failed to load WoWGatheringNodes due to "--]] 
+--[[Translation missing --]]
+--[[ L["Import WoWGatheringNodes"] = "Import WoWGatheringNodes"--]] 
+--[[Translation missing --]]
+--[[ L["Inject %s into gathering addons"] = "Inject %s into gathering addons"--]] 
+--[[Translation missing --]]
+--[[ L["Injects new objects into Gatherer/Gathermate2 that are not currently in their data files"] = "Injects new objects into Gatherer/Gathermate2 that are not currently in their data files"--]] 
+--[[Translation missing --]]
+--[[ L["Load WoWGatheringNodes and import the data to your database."] = "Load WoWGatheringNodes and import the data to your database."--]] 
+--[[Translation missing --]]
+--[[ L["Merge will add WoWGatheringNodes to your database. Overwrite will replace your database with the data in WoWGatheringNodes"] = "Merge will add WoWGatheringNodes to your database. Overwrite will replace your database with the data in WoWGatheringNodes"--]] 
+--[[Translation missing --]]
+--[[ L["WoWGatheringNodes has been imported."] = "WoWGatheringNodes has been imported."--]] 
+
+
+WoWGatheringNodes.NodeIdNames[2047] =  "真银矿石"
+WoWGatheringNodes.NodeIdNames[3729] =  "石南草"
+WoWGatheringNodes.NodeIdNames[181109] =  "[Gold Vein]"
+WoWGatheringNodes.NodeIdNames[103711] =  "锡矿"
+WoWGatheringNodes.NodeIdNames[1619] =  "地根草"
+WoWGatheringNodes.NodeIdNames[1667] =  "[Incendicite Mineral Vein]"
+WoWGatheringNodes.NodeIdNames[123309] =  "[Ooze Covered Truesilver Deposit]"
+WoWGatheringNodes.NodeIdNames[3763] =  "铜矿"
+WoWGatheringNodes.NodeIdNames[1731] =  "铜矿"
+WoWGatheringNodes.NodeIdNames[176189] =  "[Skeletal Sea Turtle]"
+WoWGatheringNodes.NodeIdNames[3764] =  "锡矿"
+WoWGatheringNodes.NodeIdNames[2653] =  "[Lesser Bloodstone Deposit]"
+WoWGatheringNodes.NodeIdNames[103712] =  "[Iron Deposit]"
+WoWGatheringNodes.NodeIdNames[1621] =  "石南草"
+WoWGatheringNodes.NodeIdNames[176587] =  "[Plaguebloom]"
+WoWGatheringNodes.NodeIdNames[123310] =  "[Ooze Covered Mithril Deposit]"
+WoWGatheringNodes.NodeIdNames[141853] =  "[Violet Tragan]"
+WoWGatheringNodes.NodeIdNames[1733] =  "银矿"
+WoWGatheringNodes.NodeIdNames[73939] =  "[Ooze Covered Iron Deposit]"
+WoWGatheringNodes.NodeIdNames[176588] =  "[Icecap]"
+WoWGatheringNodes.NodeIdNames[175207] =  "[Beached Sea Creature]"
+WoWGatheringNodes.NodeIdNames[2054] =  "[Tin Vein]"
+WoWGatheringNodes.NodeIdNames[176636] =  "[Sungrass]"
+WoWGatheringNodes.NodeIdNames[1734] =  "金矿石"
+WoWGatheringNodes.NodeIdNames[180510] =  "[Solanian's Scrying Orb]"
+WoWGatheringNodes.NodeIdNames[176589] =  "[Black Lotus]"
+WoWGatheringNodes.NodeIdNames[176637] =  "[Gromsblood]"
+WoWGatheringNodes.NodeIdNames[1735] =  "铁矿石"
+WoWGatheringNodes.NodeIdNames[180511] =  "[Scroll of Scourge Magic]"
+WoWGatheringNodes.NodeIdNames[150079] =  "[Mithril Deposit]"
+WoWGatheringNodes.NodeIdNames[176638] =  "[Golden Sansam]"
+WoWGatheringNodes.NodeIdNames[103714] =  "[Copper Vein]"
+WoWGatheringNodes.NodeIdNames[180512] =  "[Solanian's Journal]"
+WoWGatheringNodes.NodeIdNames[323] =  "[Arcanite Lode]"
+WoWGatheringNodes.NodeIdNames[150080] =  "[Gold Vein]"
+WoWGatheringNodes.NodeIdNames[176639] =  "[Dreamfoil]"
+WoWGatheringNodes.NodeIdNames[1610] =  "[Incendicite Mineral Vein]"
+WoWGatheringNodes.NodeIdNames[73941] =  "[Ooze Covered Gold Vein]"
+WoWGatheringNodes.NodeIdNames[150081] =  "[Truesilver Deposit]"
+WoWGatheringNodes.NodeIdNames[176640] =  "[Mountain Silversage]"
+WoWGatheringNodes.NodeIdNames[142142] =  "[Sungrass]"
+WoWGatheringNodes.NodeIdNames[176586] =  "[Mountain Silversage]"
+WoWGatheringNodes.NodeIdNames[252] =  "[I Should not be here B]"
+WoWGatheringNodes.NodeIdNames[181068] =  "小型黑曜石碎块"
+WoWGatheringNodes.NodeIdNames[123848] =  "[Ooze Covered Thorium Vein]"
+WoWGatheringNodes.NodeIdNames[2040] =  "秘银矿脉"
+WoWGatheringNodes.NodeIdNames[150082] =  "[Small Thorium Vein]"
+WoWGatheringNodes.NodeIdNames[176641] =  "[Plaguebloom]"
+WoWGatheringNodes.NodeIdNames[142144] =  "[Ghost Mushroom]"
+WoWGatheringNodes.NodeIdNames[142143] =  "[Blindweed]"
+WoWGatheringNodes.NodeIdNames[142145] =  "格罗姆之血"
+WoWGatheringNodes.NodeIdNames[1628] =  "墓地苔"
+WoWGatheringNodes.NodeIdNames[142141] =  "[Arthas' Tears]"
+WoWGatheringNodes.NodeIdNames[2041] =  "活根草"
+WoWGatheringNodes.NodeIdNames[176753] =  "[Doom Weed]"
+WoWGatheringNodes.NodeIdNames[176642] =  "[Arthas' Tears]"
+WoWGatheringNodes.NodeIdNames[142140] =  "紫莲花"
+WoWGatheringNodes.NodeIdNames[175404] =  "[Rich Thorium Vein]"
+WoWGatheringNodes.NodeIdNames[175928] =  "[Incendia Agave]"
+WoWGatheringNodes.NodeIdNames[2866] =  "[Firebloom]"
+WoWGatheringNodes.NodeIdNames[2044] =  "[Wintersbite]"
+WoWGatheringNodes.NodeIdNames[324] =  "瑟银矿脉"
+WoWGatheringNodes.NodeIdNames[1622] =  "跌打草"
+WoWGatheringNodes.NodeIdNames[176643] =  "[Small Thorium Vein]"
+WoWGatheringNodes.NodeIdNames[1732] =  "锡矿"
+WoWGatheringNodes.NodeIdNames[2042] =  "枯叶草"
+WoWGatheringNodes.NodeIdNames[1624] =  "皇血草"
+WoWGatheringNodes.NodeIdNames[165658] =  "黑铁矿脉"
+WoWGatheringNodes.NodeIdNames[1623] =  "野钢花"
+WoWGatheringNodes.NodeIdNames[2043] =  "卡德加的胡须"
+WoWGatheringNodes.NodeIdNames[3730] =  "[Bruiseweed]"
+WoWGatheringNodes.NodeIdNames[176644] =  "[Rich Thorium Vein]"
+WoWGatheringNodes.NodeIdNames[123308] =  "[Ooze Covered Arcanite Lode]"
+WoWGatheringNodes.NodeIdNames[103709] =  "[Tin Vein]"
+WoWGatheringNodes.NodeIdNames[1620] =  "魔皇草"
+WoWGatheringNodes.NodeIdNames[3727] =  "魔皇草"
+WoWGatheringNodes.NodeIdNames[3726] =  "地根草"
+WoWGatheringNodes.NodeIdNames[13874] =  "毒蛇花"
+WoWGatheringNodes.NodeIdNames[105569] =  "[Silver Vein]"
+WoWGatheringNodes.NodeIdNames[176645] =  "[Mithril Deposit]"
+WoWGatheringNodes.NodeIdNames[177388] =  "[Ooze Covered Rich Thorium Vein]"
+WoWGatheringNodes.NodeIdNames[35253] =  "[Mining Node Point Check]"
+WoWGatheringNodes.NodeIdNames[3724] =  "宁神花"
+WoWGatheringNodes.NodeIdNames[175566] =  "[Gloom Weed]"
+WoWGatheringNodes.NodeIdNames[73940] =  "[Ooze Covered Silver Vein]"
+WoWGatheringNodes.NodeIdNames[2045] =  "荆棘藻"
+WoWGatheringNodes.NodeIdNames[3725] =  "银叶草"
+WoWGatheringNodes.NodeIdNames[103713] =  "[Copper Vein]"
+WoWGatheringNodes.NodeIdNames[180600] =  "[Unstable Mana Crystal Crate]"
+WoWGatheringNodes.NodeIdNames[103710] =  "[Iron Deposit]"
+WoWGatheringNodes.NodeIdNames[1617] =  "银叶草"
+WoWGatheringNodes.NodeIdNames[176583] =  "[Golden Sansam]"
+WoWGatheringNodes.NodeIdNames[19903] =  "[Indurium Mineral Vein]"
+WoWGatheringNodes.NodeIdNames[2046] =  "金棘草"
+WoWGatheringNodes.NodeIdNames[13891] =  "[Serpentbloom]"
+WoWGatheringNodes.NodeIdNames[2055] =  "[Copper Vein]"
+WoWGatheringNodes.NodeIdNames[181108] =  "[Truesilver Deposit]"
+WoWGatheringNodes.NodeIdNames[35254] =  "[Herb Node Point Check]"
+WoWGatheringNodes.NodeIdNames[1618] =  "宁神花"
+WoWGatheringNodes.NodeIdNames[176584] =  "[Dreamfoil]"
+WoWGatheringNodes.NodeIdNames[181069] =  "大型黑曜石碎块"
