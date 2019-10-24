da354987b6a58825c2f3eff019a707ea2973878c
Js <JimmyLoveSiren@users.noreply.github.com>
2019-10-24 23:51:48 +0900

    Questie v5.0.1

--- Interface/AddOns/Questie/Database/zoneDB.lua
+++ Interface/AddOns/Questie/Database/zoneDB.lua
@@ -97,8 +97,8 @@ local zoneDataClassicBetaHack = {
     ["Alterac Valley"] = {1459,947},
     ["Warsong Gulch"] = {1460,947},
     ["Arathi Basin"] = {1461,947},
-    ["Eastern Kingdoms"] = {1463,0},
-    ["Kalimdor"] = {1464,0}
+    --["Eastern Kingdoms"] = {1463,0},
+    --["Kalimdor"] = {1464,0}
 }
 
 
@@ -429,7 +429,7 @@ instanceData = {
 
 
 
-function QuestieDBZone:zoneCreateConvertion()
+function QuestieDBZone:ZoneCreateConversion()
   Questie:Debug(DEBUG_DEVELOP, "[QuestieDBZone] Converting ZoneIds")
     for index, Data in ipairs(zoneDataClassicDemo) do
         local buildNr = select(4, GetBuildInfo())
