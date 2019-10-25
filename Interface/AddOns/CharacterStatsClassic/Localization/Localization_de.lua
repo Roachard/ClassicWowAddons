9d668db2483f1f0d144563ae71163716512d26af
Js <JimmyLoveSiren@users.noreply.github.com>
2019-10-25 23:34:28 +0900

    BigFoot v1.13.2.23

--- Interface/AddOns/CharacterStatsClassic/Localization/Localization_de.lua
+++
@@ -1,5 +0,0 @@
-if GetLocale() ~= "deDE" then return end
-
--- Translate the strings here
-CSC_DEFENSE                 = DEFENSE;
-CSC_WEAPON_SKILLS_HEADER    = "Weapon Skills";
--- Interface/AddOns/CharacterStatsClassic/Localization/Localization_ru.lua
+++
@@ -1,5 +0,0 @@
-if GetLocale() ~= "ruRU" then return end
-
--- Translate the strings here
-CSC_DEFENSE                 = DEFENSE;
-CSC_WEAPON_SKILLS_HEADER    = "Оружейные навыки";
