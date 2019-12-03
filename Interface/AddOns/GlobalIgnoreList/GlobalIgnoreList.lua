a78b5b5adff1d2e25b5b1fa25c3c83069542dada
Js <JimmyLoveSiren@users.noreply.github.com>
2019-12-03 23:51:03 +0900

    GlobalIgnoreList

--- Interface/AddOns/GlobalIgnoreList/GlobalIgnoreList.lua
+++ Interface/AddOns/GlobalIgnoreList/GlobalIgnoreList.lua
@@ -650,7 +650,11 @@ local function ApplicationStartup(self)
 	filterDefActive[#filterDefActive + 1] = true
 
 	filterDefDesc[#filterDefDesc + 1]     = "飞机"
-	filterDefFilter[#filterDefFilter + 1] = "(([contains=飞] or [contains=专] or [contains=票] or [contains=达] or [contains=通]) and ([contains=机] or [contains=直])) or ([contains=航] and ([contains=空] or [contains=班])) or (([contains=G] or [contains=金]) and ([contains=拉] or [contains=啦] or [contains=秒到] or [contains=加基森] or [contains=冬泉谷] or [contains=厄运]))"
+	filterDefFilter[#filterDefFilter + 1] = "(([contains=飞] or [contains=专] or [contains=票] or [contains=达] or [contains=通]) and ([contains=机] or [contains=直])) or ([contains=航] and ([contains=空] or [contains=班])) or (([contains=G] or [contains=金]) and ([contains=拉] or [contains=啦] or [contains=秒到] or [contains=加基森] or [contains=冬泉谷] or [contains=厄运] or [contains=斯坦索姆] or [contains=STSM]))"
+	filterDefActive[#filterDefActive + 1] = true
+
+	filterDefDesc[#filterDefDesc + 1]     = "赌博"
+	filterDefFilter[#filterDefFilter + 1] = "([contains=RO点] or [contains=R0点]) and not [channel=s]"
 	filterDefActive[#filterDefActive + 1] = true
 
 	filterDefDesc[#filterDefDesc + 1]     = "金商"
